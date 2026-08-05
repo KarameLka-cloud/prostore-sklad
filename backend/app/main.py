7c4be869d6458009dad066255fb14ac0fbbdb58d
Valentin <>
2026-08-05 23:58:06 +0800

    Развернута база для backend

---
+++ backend/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+def main() -> None:
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
