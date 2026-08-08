80a7be498bcd0b7554af9d3103f271fc29b050e0
Valentin <>
2026-08-08 22:14:30 +0800

    Мелкие правки

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,13 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "world"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-
-def main() -> None:
+def main():
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
